ca88dafe766599f0cff96f4010fb869cdbbbc5a6
Yaroslav Nechaev <remper@me.com>
2016-01-28 19:08:01 +0100

    Added an ability to find similar tokens in known word vectors

--- src/main/java/eu/fbk/hlt/data/WordVectors.java
+++ src/main/java/eu/fbk/hlt/data/WordVectors.java
@@ -1,15 +1,23 @@
 package eu.fbk.hlt.data;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import edu.stanford.nlp.util.Generics;
+import eu.fbk.hlt.sentiment.util.DatasetProvider;
+import eu.fbk.hlt.sentiment.util.SentimentParameters;
+import eu.fbk.hlt.sentiment.util.Stopwatch;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.collections.SortedBag;
 import org.ejml.simple.SimpleMatrix;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.ops.transforms.Transforms;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.URISyntaxException;
-import java.util.Map;
+import java.util.*;
 
 /**
  * A word vector lookup map, mainly used to store word embeddings
@@ -79,6 +87,28 @@ public class WordVectors extends Dataset {
         }
     }
 
+    public Map<Double, String> findSimilar(String word) {
+        INDArray initial = wordVectors.getOrDefault(word, zeroes);
+        TreeMap<Double, String> scores = new TreeMap<>();
+        Iterator<Map.Entry<String, INDArray>> iterator = wordVectors.entrySet().iterator();
+        int counter = 0;
+        while (iterator.hasNext()) {
+            Map.Entry<String, INDArray> entry = iterator.next();
+            INDArray value = entry.getValue();
+            double similarity = Transforms.cosineSim(value, initial);
+            if (scores.size() < 10 || similarity > scores.firstKey()) {
+                scores.put(similarity, entry.getKey());
+                if (scores.size() == 11) {
+                    scores.remove(scores.firstKey());
+                }
+            }
+            if (++counter % 100000 == 0) {
+                logger.debug("Looked up "+counter+" words");
+            }
+        }
+        return scores.descendingMap();
+    }
+
     public INDArray lookup(String word) {
         return wordVectors.getOrDefault(word, zeroes);
     }
@@ -89,5 +119,39 @@ public class WordVectors extends Dataset {
 
     public int getDim() {
         return dim;
+    }
+
+    /**
+     * Here we automatically bootstrap
+     *  our environment with just a little bit
+     *  of a custom WordVectors construction
+     *  via the DatasetProvider class
+     */
+    public static void main(String[] args) throws ParseException {
+        SentimentParameters params = new SentimentParameters(args);
+        Injector injector = Guice.createInjector(new DatasetProvider(params));
+        WordVectors wordVectors = injector.getInstance(WordVectors.class);
+        wordVectors.interactive();
+    }
+
+    public void interactive() {
+        logger.info("Enabling interactive mode");
+
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(System.in))) {
+            while (true) {
+                System.out.println("Please write a token:");
+                Stopwatch watch = Stopwatch.start();
+                String token = input.readLine().toLowerCase();
+                Map<Double, String> result = this.findSimilar(token);
+                System.out.println("Found similar tokens for \""+token+"\" in "+watch.click()+" ms");
+                System.out.println("Similar:");
+                for (Map.Entry<Double, String> score : result.entrySet()) {
+                    System.out.println(" "+score.getValue()+" — "+score.getKey());
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            logger.error("Error while reading from stream or closing", e);
+        }
     }
 }
